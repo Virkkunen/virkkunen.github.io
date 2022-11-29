94150f555c77265814e2d5d9ad4f5cd17369646f
Virkkunen <vrkknn@vrkknn.net>
2022-11-29 14:15:44 -0300

    css styling

---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Main</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p>Aliquid placeat odit molestiae hic, id nostrum unde quaerat cupiditate voluptatibus recusandae nam veritatis?</p>
+        <p>Distinctio obcaecati nobis voluptatem nostrum corporis maxime minus?</p>
+      </div>
+    )
+  }
+}
