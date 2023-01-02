0db3db58110feeb862e32722fa0fa90c34532e4a
Fred Schauß <vrkknn@vrkknn.net>
2023-01-02 09:41:47 -0300

    css styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className='header-name'>
+          <span>Fred</span> <span className='nick'>"Virkkunen"</span>
+          <p>Almeida</p>
+        </div>
+        <FontAwesomeIcon icon={faUser} />
+      </header>
+    )
+  }
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Header from '../components/Header'
 
 export default class Main extends Component {
   render() {
     return (
-      <div className='content'>Main</div>
+      <div className='content'>
+        <Header />
+      </div>
     )
   }
 }
