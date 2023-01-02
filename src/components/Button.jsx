c6a04fb58ee2fa9a4d1f6ec304e96eebcba2e03c
Fred Schauß <vrkknn@vrkknn.net>
2023-01-02 10:39:09 -0300

    button logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 
 export default class Button extends Component {
+  handleClick = () => {
+    const { link } = this.props;
+    window.location.assign(link);
+  };
+
   render() {
-    const { icon, iconType, name, link, color } = this.props;
+    const { icon, iconType, name, color } = this.props;
     return (
       <button
         type='button'
         name={ name }
         className={ color }
+        onClick={this.handleClick}
       >
         <FontAwesomeIcon icon={[iconType, icon]} size="lg"/>
         { name }
