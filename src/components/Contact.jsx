dc322eede9e82df6ce9c007f40e1bc912a96e010
Fred Schauß <vrkknn@vrkknn.net>
2023-01-03 09:41:53 -0300

    buttongroup on contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,11 @@
 import React, { Component } from "react";
-import Button from "./Button";
+import ButtonGroup from "./ButtonGroup";
 export default class Contact extends Component {
   render() {
-    const json = require("../buttons.json");
-    const buttons = json.contact;
-
     return (
       <div className="box">
         <h1>Contact me:</h1>
-        <div className="buttons">
-          {buttons.map((link) => (
-            <Button
-              key={link.name}
-              name={link.name}
-              icon={link.icon}
-              iconType={link.iconType}
-              link={link.icon}
-              color={link.color}
-            />
-          ))}
-        </div>
+        <ButtonGroup type="contact" />
       </div>
     );
   }
