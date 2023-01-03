93bc270ae254e9c818ada19a5b2ff00e4df8d21c
Fred Schauß <vrkknn@vrkknn.net>
2023-01-03 09:27:39 -0300

    buttons now mapped from json file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,49 +1,26 @@
-import React, { Component } from 'react'
-import Button from './Button'
-
+import React, { Component } from "react";
+import Button from "./Button";
 export default class Contact extends Component {
   render() {
+    const json = require("../buttons.json");
+    const buttons = json.contact;
+
     return (
-      <div className='box'>
+      <div className="box">
         <h1>Contact me:</h1>
-        <div className='buttons'>
-          <Button
-            name="fredalmeida-"
-            icon="linkedin"
-            iconType="fab"
-            link="https://www.linkedin.com/in/fredalmeida-/"
-            color="blue"
-          />
-          <Button
-            name="fred@vrkknn.net"
-            icon="envelope"
-            iconType="fas"
-            link="mailto:vrkknn@vrkknn.net"
-            color="mauve"
-          />
-          <Button
-            name="@vrkknn"
-            icon="telegram"
-            iconType="fab"
-            link="https://t.me/vrkknn"
-            color="sapphire"
-          />
-          <Button
-            name="Virkkunen"
-            icon="github"
-            iconType="fab"
-            link="https://github.com/Virkkunen"
-            color="grey"
-          />
-          <Button
-            name="WhatsApp"
-            icon="whatsapp"
-            iconType="fab"
-            link="https://wa.link/t1uuz0"
-            color="green"
-          />
+        <div className="buttons">
+          {buttons.map((link) => (
+            <Button
+              key={link.name}
+              name={link.name}
+              icon={link.icon}
+              iconType={link.iconType}
+              link={link.icon}
+              color={link.color}
+            />
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
