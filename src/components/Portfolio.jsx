b36bdeace2b4c4ad22112ddcb8cba36b35a86475
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:33:11 -0300

    hooked Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import ButtonGroup from "./ButtonGroup";
 
-export default class Portfolio extends Component {
-  render() {
-    return (
-      <div className="box hidden">
-        <h1>Stuff I made</h1>
-        <ButtonGroup type="portfolio" />
-      </div>
-    );
-  }
+export default function Portfolio() {
+  return (
+    <div className="box hidden">
+      <h1>Stuff I made</h1>
+      <ButtonGroup type="portfolio" />
+    </div>
+  );
 }
