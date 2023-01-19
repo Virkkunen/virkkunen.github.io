a0590a2988b16c6b473cc43df096ebf19c3a9336
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:32:36 -0300

    hooked Languages

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
-import Devicon from './Devicon';
-// import { ReactOriginal, Html5Original, Css3Original, JavascriptOriginal, PythonOriginal, GitOriginal, BootstrapOriginal, MarkdownOriginal, JiraOriginal, DockerOriginal, DjangoPlain, VscodeOriginal, PhotoshopLine, PremiereproOriginal, LinuxOriginal, Windows8Original, AppleOriginal, AndroidOriginal, BashOriginal, GithubOriginal, RaspberrypiOriginal, ReduxOriginal, TrelloPlain } from 'devicons-react'
+import React from "react";
+import Devicon from "./Devicon";
 
-export default class Languages extends Component {
-  render() {
-    const size = "48px";
-    const json = require("../data/buttons.json");
-    const icons = json.devicons;
-
-    return (
-      <div className='box hidden'>
-        <h1>Stuff I know</h1>
-        <div className='buttons-small'>
-          {icons.map((icon) => (
-            <Devicon
-              name={icon.name}
-              iconStyle={icon.iconStyle}
-              svgStyle={icon.svgStyle}
-              size={size}
-              key={icon.name}
-              svg={true}
-            />
-          ))}
-        </div>
+export default function Languages() {
+  const size = "48px";
+  const json = require("../data/buttons.json");
+  const icons = json.devicons;
+  return (
+    <div className="box hidden">
+      <h1>Stuff I know</h1>
+      <div className="buttons-small">
+        {icons.map((icon) => (
+          <Devicon
+            name={icon.name}
+            iconStyle={icon.iconStyle}
+            svgStyle={icon.svgStyle}
+            size={size}
+            key={icon.name}
+            svg={true}
+          />
+        ))}
       </div>
-    )
-  }
+    </div>
+  );
 }
