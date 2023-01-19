fd040aaa495755aec26bbb8e57378e2bc71281ee
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:18:15 -0300

    hooked Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,19 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { Component } from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default class Button extends Component {
-  render() {
-    const { icon, iconType, name, color, link } = this.props;
-    return (
-      <a
-        type='button'
-        name={ name }
-        className={ color }
-        href={ link }
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        { icon && <FontAwesomeIcon icon={[iconType, icon]} size="lg"/> }
-        { name }
-      </a>
-    )
-  }
+export default function Button(props) {
+  const { icon, iconType, name, color, link } = props;
+  return (
+    <a
+      type="button"
+      name={ name }
+      className={ color }
+      href={ link }
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      { icon && <FontAwesomeIcon icon={[iconType, icon]} size="lg" /> }
+      { name }
+    </a>
+  );
 }
- 
