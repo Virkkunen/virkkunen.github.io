bf30c6777b0b2c0bf4dfeee85ee8ec7a022d24c1
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:22:54 -0300

    hooked CV

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from './Button'
 
-export default class CV extends Component {
-  render() {
-    return (
-      <div className='box hidden'>
-        <h1>Curriculum</h1>
-        <div className='buttons'>
-          <Button
-            name="🇧🇷 Currículo"
-            color="green-bold"
-            link="https://github.com/Virkkunen/virkkunen.github.io/raw/master/assets/files/cv_ptBR.pdf"
-          />
-          <Button
-            name="🇬🇧 Curriculum Vitae"
-            color="blue-bold"
-            link="https://github.com/Virkkunen/virkkunen.github.io/raw/master/assets/files/cv_enGB.pdf"
-          />
-        </div>
-      </div>
-    )
-  }
+export default function CV() {
+  return (
+    <div className='box hidden'>
+    <h1>Curriculum</h1>
+    <div className='buttons'>
+      <Button
+        name="🇧🇷 Currículo"
+        color="green-bold"
+        link="https://github.com/Virkkunen/virkkunen.github.io/raw/master/assets/files/cv_ptBR.pdf"
+      />
+      <Button
+        name="🇬🇧 Curriculum Vitae"
+        color="blue-bold"
+        link="https://github.com/Virkkunen/virkkunen.github.io/raw/master/assets/files/cv_enGB.pdf"
+      />
+    </div>
+  </div>
+  )
 }
