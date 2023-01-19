cfa823e99ea3327ddb907be6d28b66814804d88e
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:27:16 -0300

    hooked Devicon

--- src/components/Devicon.jsx
+++ src/components/Devicon.jsx
@@ -1,41 +1,30 @@
-import React, { Component } from 'react'
-import 'devicon';
+import React from "react";
+import "devicon";
 
-export default class Devicon extends Component {
-  render() {
-    const { name, iconStyle, svgStyle = 'original', size = '1rem', color = '', svg } = this.props;
-    
-    if (svg) {
-      const srcLink = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${name}/${name}-${svgStyle}.svg`;
-      return (
-        <img
-          src={srcLink}
-          alt={name}
-          width={size}
-          height={size}
-          title={name}
-        />
-      )
-    };
-    
-    // <i class="devicon-adonisjs-original"></i>
-    // <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/raspberrypi/raspberrypi-original.svg" />
-    // <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
-          
-    const iconClass = `devicon-${name}-${iconStyle} colored`;
-   
-    let style = {
-      fontSize: size,
-    };
-    if (size) style.fontSize = size;
-    if (color) style.color = color;
+export default function Devicon(props) {
+  const {
+    name,
+    iconStyle,
+    svgStyle = "original",
+    size = "1rem",
+    color = "",
+    svg,
+  } = props;
 
+  if (svg) {
+    const srcLink = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${name}/${name}-${svgStyle}.svg`;
     return (
-      <i
-        className={iconClass}
-        style={style}
-        title={name}
-      />
-    )
+      <img src={srcLink} alt={name} width={size} height={size} title={name} />
+    );
   }
+
+  const iconClass = `devicon-${name}-${iconStyle} colored`;
+
+  let style = {
+    fontSize: size,
+  };
+  if (size) style.fontSize = size;
+  if (color) style.color = color;
+
+  return <i className={iconClass} style={style} title={name} />;
 }
