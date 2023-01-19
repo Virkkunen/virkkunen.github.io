e660a97cede0df61a9f44df2327472766a9ca684
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:28:30 -0300

    hooked Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,28 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div className='footer hidden'>
-        <a
-          className='footer-item a-footer'
-          href="https://github.com/Virkkunen"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
-          <span>@Virkkunen</span>
-        </a>
-        <span>•</span>
-        <a
-          className='footer-item a-footer'
-          href="https://github.com/Virkkunen/virkkunen.github.io"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <FontAwesomeIcon icon={['fas', 'code']} size="lg" />
-          <span>source</span>
-        </a>
-      </div>
-    )
-  }
+export default function Footer() {
+  return (
+    <div className="footer hidden">
+      <a
+        className="footer-item a-footer"
+        href="https://github.com/Virkkunen"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <FontAwesomeIcon icon={["fab", "github"]} size="lg" />
+        <span>@Virkkunen</span>
+      </a>
+      <span>•</span>
+      <a
+        className="footer-item a-footer"
+        href="https://github.com/Virkkunen/virkkunen.github.io"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <FontAwesomeIcon icon={["fas", "code"]} size="lg" />
+        <span>source</span>
+      </a>
+    </div>
+  );
 }
