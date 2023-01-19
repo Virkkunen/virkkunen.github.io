0d773ba18746063e55c56ca5508dcf5c849a340a
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:21:33 -0300

    hooked Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React from 'react'
 import ButtonGroup from "./ButtonGroup";
-export default class Contact extends Component {
-  render() {
-    return (
-      <div className="box hidden">
+
+export default function Contact() {
+  return (
+    <div className="box hidden">
         <h1>Contact me</h1>
         <ButtonGroup type="contact" />
       </div>
-    );
-  }
+  )
 }
