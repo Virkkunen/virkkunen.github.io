1d86343404e524083a9c9e90a56149218a44e34e
Fred Schauß <vrkknn@vrkknn.net>
2023-01-18 22:20:24 -0300

    hooked ButtonGroup

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from "./Button";
 
-export default class ButtonGroup extends Component {
-  render() {
-    const { type } = this.props;
-    const json = require("../data/buttons.json");
-    const buttons = (json[type]);
-    return (
-      <div className='buttons'>
-        {buttons.map((link) => (
+export default function ButtonGroup({ type }) {
+  const json = require("../data/buttons.json");
+  const buttons = json[type];
+  return (
+    <div className='buttons'>
+        { buttons.map((link) => (
             <Button
-              key={link.name}
-              name={link.name}
-              icon={link.icon}
-              iconType={link.iconType}
-              link={link.link}
-              color={link.color}
+              key={ link.name }
+              name={ link.name }
+              icon={ link.icon }
+              iconType={ link.iconType }
+              link={ link.link }
+              color={ link.color }
             />
-          ))}
+          )) }
       </div>
-    )
-  }
+  )
 }
