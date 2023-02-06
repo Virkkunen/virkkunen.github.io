4e336e7a57dd57a67d90271cb1996ab762e03bdb
Virkkunen <vrkknn@vrkknn.net>
2023-02-06 17:10:31 -0300

    more icons

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faCode, faClock, faDiceD20 } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faCode, faClock, faDiceD20, faBowlFood } from "@fortawesome/free-solid-svg-icons";
 import AppProvider from "./context/AppProvider";
 
-library.add(fab, faEnvelope, faCode, faClock, faDiceD20);
+library.add(fab, faEnvelope, faCode, faClock, faDiceD20, faBowlFood);
 
 function App() {
   return (
