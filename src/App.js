222d60bc227fc3d5d1b89937aac5bcb79045b390
Virkkunen <vrkknn@vrkknn.net>
2023-03-28 19:14:38 -0300

    feat: add weather app icon

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore, faCloudMoon } from "@fortawesome/free-solid-svg-icons";
 import AppProvider from "./context/AppProvider";
 
-library.add(fab, faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore);
+library.add(fab, faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore, faCloudMoon);
 
 function App() {
   return (
