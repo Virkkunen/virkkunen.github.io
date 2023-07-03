5b36d457f66a86209130d2de7d6e7f2e6a263f72
Virkkunen <vrkknn@vrkknn.net>
2023-07-03 18:15:46 -0300

    small update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faMastodon, fab } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore, faCloudMoon } from "@fortawesome/free-solid-svg-icons";
 import AppProvider from "./context/AppProvider";
 
-library.add(fab, faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore, faCloudMoon);
+library.add(fab, faEnvelope, faCode, faClock, faDiceD20, faBowlFood, faStore, faCloudMoon, faMastodon);
 
 function App() {
   return (
