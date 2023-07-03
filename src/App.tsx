6c4817c02d5a75ebcaab2e9b3ff74ba0748b5376
Virkkunen <vrkknn@vrkknn.net>
2023-07-03 20:51:47 -0300

    feat: buttons, fontawesome, block

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import { FaCat } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Block from './components/Block';
+import Button from './components/Button';
+import { faCat } from '@fortawesome/free-solid-svg-icons';
+import data from '../public/data';
 
 function App() {
   return (
-    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen'>
-      <div className='flex flex-row justify-between p-6 bg-base rounded-2xl items-center xl:col-start-4 xl:col-span-6'>
-        <div className='flex flex-col gap-y-1 font-extrabold text-[48px]'>
-          <div className='flex flex-row gap-x-2 items-baseline'>
-            <span>Fred</span>
-            <span className='text-subtext0 text-xl'>'Virkkunen'</span>
+    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8'>
+      <Block>
+        <div className='flex flex-row justify-between items-center'>
+          <div className='flex flex-col gap-y-1 font-extrabold text-[48px]'>
+            <div className='flex flex-row gap-x-2 items-baseline'>
+              <span>Fred</span>
+              <span className='text-subtext0 text-xl'>'Virkkunen'</span>
+            </div>
+            <span>Almeida</span>
           </div>
-          <span>Almeida</span>
-          <span className='text-subtext1 text-xl max-xl:text-lg font-medium mt-6'>🇧🇷🇮🇹 Italian-Brazilian Web Developer based in 🇭🇺 Hungary</span>
+          <FontAwesomeIcon
+            icon={faCat}
+            color='#d20f39'
+          />
         </div>
-        <FaCat size='4em' color='#d20f39'/>
-      </div>
+        <span className='text-subtext1 text-xl max-sm:text-sm font-medium mt-6'>
+          🇧🇷🇮🇹Italian-Brazilian Web Developer based in 🇭🇺 Hungary
+        </span>
+      </Block>
+
+      <Block
+        title='Contact me:'
+        buttonGroup
+      >
+        {data.contact.map((item) => (
+          <Button
+            title={item.title}
+            icon={item.icon}
+            color={item.color}
+            link={item.link}
+          />
+        ))}
+      </Block>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,47 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface Button {
+  title: string;
+  icon: IconProp;
+  color: string;
+  link: string;
+}
+
+const Button: React.FC<Button> = ({ title, icon, color, link }) => {
+  // definitely there's a better way of doing this
+  const bgColor: { [key: string]: string } = {
+    rosewater: 'bg-rosewater',
+    flamingo: 'bg-flamingo',
+    pink: 'bg-pink',
+    mauve: 'bg-mauve',
+    red: 'bg-red',
+    maroon: 'bg-maroon',
+    peach: 'bg-peach',
+    yellow: 'bg-yellow',
+    green: 'bg-green',
+    teal: 'bg-teal',
+    sky: 'bg-sky',
+    sapphire: 'bg-sapphire',
+    blue:'bg-blue',
+    lavender: 'bg-lavender',
+    grey: 'bg-overlay2'
+  };
+
+  return (
+    <a
+      className={`${bgColor[color]} px-6 py-4 xl:px-4 xl:py-2 rounded-2xl shadow-sm text-crust font-semibold font-fira-condensed transition-all ease-in-out duration-200 hover:brightness-110 active:brightness-75 flex flex-row justify-between gap-x-2 items-center text-2xl xl:text-xl`}
+      target='_blank'
+      rel='noreferrer noopener'
+      href={link}
+    >
+      <FontAwesomeIcon
+        icon={icon}
+        fixedWidth
+      />
+      <span>{title}</span>
+    </a>
+  );
+};
+
+export default Button;
