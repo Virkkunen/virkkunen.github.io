52e830022c5c1ca0a710a2e2a26d57991b3c68ee
Virkkunen <vrkknn@vrkknn.net>
2023-07-03 22:34:58 -0300

    feat: vertical padding on xl

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
-    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8'>
+    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48'>
       <Block>
         {/* I'll get around simplifying this eventually */}
         <div className='flex flex-row justify-between items-center'>
