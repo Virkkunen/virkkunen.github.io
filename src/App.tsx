e1e3d628b56f9b41b501d10752c472f89bfd2e7f
Virkkunen <vrkknn@vrkknn.net>
2024-04-23 11:05:34 +0200

    animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,71 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Block from './components/Block';
-import Button from './components/Button';
-import { faCat, faCode } from '@fortawesome/free-solid-svg-icons';
-import data from './data';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Block from "./components/Block";
+import Button from "./components/Button";
+import { faCat, faCode } from "@fortawesome/free-solid-svg-icons";
+import data from "./data";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { LazyMotion, MotionConfig, domAnimation } from "framer-motion";
 
 function App() {
   return (
-    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48'>
-      <Block>
-        {/* I'll get around simplifying this eventually */}
-        <div className='flex flex-row justify-between items-center'>
-          <div className='flex flex-col gap-y-1 font-extrabold text-[48px]'>
-            <div className='flex flex-row gap-x-2 items-baseline'>
-              <span>Fred</span>
-              <span className='text-subtext0 text-xl'>'Virkkunen'</span>
+    <MotionConfig reducedMotion="user">
+      <LazyMotion features={domAnimation}>
+        <div className="flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48">
+          <Block>
+            {/* I'll get around simplifying this eventually */}
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-col gap-y-1 font-extrabold text-[48px]">
+                <div className="flex flex-row gap-x-2 items-baseline">
+                  <span>Fred</span>
+                  <span className="text-subtext0 text-xl">'Virkkunen'</span>
+                </div>
+                <span>Almeida</span>
+              </div>
+              <FontAwesomeIcon
+                className="transition-all ease-in-out duration-200 rounded-2xl hover:bg-clip-content hover:text-transparent hover:bg-gradient-to-b hover:from-[#5BCEFA] hover:via-[#F5A9B8] hover:to-white"
+                icon={faCat}
+                color="#d20f39"
+                size="4x"
+              />
             </div>
-            <span>Almeida</span>
-          </div>
-          <FontAwesomeIcon
-            className='transition-all ease-in-out duration-200 rounded-2xl hover:bg-clip-content hover:text-transparent hover:bg-gradient-to-b hover:from-[#5BCEFA] hover:via-[#F5A9B8] hover:to-white'
-            icon={faCat}
-            color='#d20f39'
-            size='4x'
-          />
-        </div>
-        <span className='text-subtext1 text-xl max-sm:text-sm font-medium mt-6'>
-          🇧🇷 🇮🇹 Italian-Brazilian Web Developer based in 🇭🇺 Hungary
-        </span>
-      </Block>
+            <span className="text-subtext1 text-xl max-sm:text-sm font-medium mt-6">
+              🇧🇷 🇮🇹 Italian-Brazilian Web Developer based in 🇭🇺 Hungary
+            </span>
+          </Block>
 
-      <Block
-        title='Contact me'
-        buttonGroup
-      >
-        {data.contact.map((item) => (
-          <Button
-            title={item.title}
-            icon={item.icon}
-            color={item.color}
-            link={item.link}
-            key={item.title}
-            tooltipTarget={item.title.replace(/\s/g, '')}
-            tooltip={item.tooltip}
+          <Block title="Contact me" buttonGroup>
+            {data.contact.map((item) => (
+              <Button
+                title={item.title}
+                icon={item.icon}
+                color={item.color}
+                link={item.link}
+                key={item.title}
+                tooltipTarget={item.title.replace(/\s/g, "")}
+                tooltip={item.tooltip}
+              />
+            ))}
+          </Block>
 
-          />
-        ))}
-      </Block>
+          <Block title="Some projects" buttonGroup>
+            {data.portfolio.map((item) => (
+              <Button
+                title={item.title}
+                icon={item.icon}
+                color={item.color}
+                link={item.link}
+                key={item.title}
+                tooltipTarget={item.title.replace(/\s/g, "")}
+                tooltip={item.tooltip}
+              />
+            ))}
+          </Block>
 
-      <Block
-        title="Some projects"
-        buttonGroup
-      >
-        {data.portfolio.map((item) => (
-          <Button
-            title={item.title}
-            icon={item.icon}
-            color={item.color}
-            link={item.link}
-            key={item.title}
-            tooltipTarget={item.title.replace(/\s/g, '')}
-            tooltip={item.tooltip}
-          />
-        ))}
-      </Block>
-
-      {/* <Block title='What I know:'>
+          {/* <Block title='What I know:'>
         <span className='text-[10px]'>(this section will be updated soon™️)</span>
       </Block> */}
 
-      {/* <Block
+          {/* <Block
         title='Curriculum:'
         buttonGroup
       >
@@ -87,36 +83,32 @@ function App() {
         />
       </Block> */}
 
-      <Block>
-        <div className='flex flex-row justify-around xl:mx-auto xl:gap-24'>
-          <a
-            className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue active:text-lavender'
-            target='_blank'
-            rel='noreferrer noopener'
-            href='https://github.com/Virkkunen'
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              fixedWidth
-            />
-            <span>Virkkunen</span>
-          </a>
-          <span className='text-overlay2'>•</span>
-          <a
-            className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue active:text-lavender'
-            target='_blank'
-            rel='noreferrer noopener'
-            href='https://github.com/Virkkunen/virkkunen.github.io'
-          >
-            <FontAwesomeIcon
-              icon={faCode}
-              fixedWidth
-            />
-            <span>source</span>
-          </a>
+          <Block>
+            <div className="flex flex-row justify-around xl:mx-auto xl:gap-24">
+              <a
+                className="flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue active:text-lavender"
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://github.com/Virkkunen"
+              >
+                <FontAwesomeIcon icon={faGithub} fixedWidth />
+                <span>Virkkunen</span>
+              </a>
+              <span className="text-overlay2">•</span>
+              <a
+                className="flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue active:text-lavender"
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://github.com/Virkkunen/virkkunen.github.io"
+              >
+                <FontAwesomeIcon icon={faCode} fixedWidth />
+                <span>source</span>
+              </a>
+            </div>
+          </Block>
         </div>
-      </Block>
-    </div>
+      </LazyMotion>
+    </MotionConfig>
   );
 }
 
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { m } from "framer-motion";
+import { ReactNode } from "react";
 
 interface Block {
   children: ReactNode;
@@ -6,14 +7,49 @@ interface Block {
   buttonGroup?: boolean;
 }
 
+const animList = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    // transition: { ease: [0.6, -0.05, 0.01, 0.99], duration: 0.56 },
+    transition: {
+      ease: "circInOut",
+      duration: 0.56,
+      type: "spring",
+    },
+  },
+  hidden: { opacity: 0, y: 2000 },
+};
+
+const animItem = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { ease: "easeInOut", duration: 0.8 },
+  },
+  hidden: { opacity: 0 },
+};
+
 const Block: React.FC<Block> = ({ children, title, buttonGroup }) => {
   return (
-    <div
+    <m.div
       className={`flex flex-col justify-between p-6 bg-base rounded-2xl xl:col-start-4 xl:col-span-6 shadow-md`}
+      initial="hidden"
+      animate="visible"
+      variants={animList}
     >
-      {title && <span className='font-bold text-2xl mb-6'>{title}</span>}
-      {buttonGroup ? <div className='flex max-lg:flex-col max-lg:justify-between flex-wrap gap-x-2 gap-y-4 xl:gap-4'>{children}</div> : <>{children}</>}
-    </div>
+      {title && <span className="font-bold text-2xl mb-6">{title}</span>}
+      {buttonGroup ? (
+        <m.div
+          className="flex max-lg:flex-col max-lg:justify-between flex-wrap gap-x-2 gap-y-4 xl:gap-4"
+          variants={animItem}
+        >
+          {children}
+        </m.div>
+      ) : (
+        <>{children}</>
+      )}
+    </m.div>
   );
 };
 
