7c5755a5726f1175df095d5ef21a94fece83efc1
Virkkunen <vrkknn@vrkknn.net>
2025-01-04 13:11:54 +0100

    pwa test

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: '.well-known/matrix',
-    //       dest: '.well-known/'
-    //     }
-    //   ]
-    // })
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   base: '/',
   build: {
